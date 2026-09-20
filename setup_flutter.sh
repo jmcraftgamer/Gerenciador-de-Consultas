92b4562190ddcd996d94f7de71c61943a40c9371
jmcraftgamer <jmcraftgamerplayerr@gmail.com>
2026-09-20 20:29:39 -0300

    fix: wget + bash script for vercel

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -3,20 +3,16 @@ set -e
 
 echo "Installing Flutter SDK..."
 
-# Download and extract Flutter
 cd /tmp
-curl -fsSL -o flutter.tar.xz "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.44.8-stable.tar.xz"
+wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.44.8-stable.tar.xz -O flutter.tar.xz
 tar xf flutter.tar.xz
 rm flutter.tar.xz
 export PATH="/tmp/flutter/bin:$PATH"
 
 echo "Flutter installed: $(flutter --version | head -1)"
 
-# Configure Flutter
 flutter config --no-analytics
-flutter doctor -v
 
-# Get dependencies
 cd "$OLDPWD"
 flutter pub get
 
