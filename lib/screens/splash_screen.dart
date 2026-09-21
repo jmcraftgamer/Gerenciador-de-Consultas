79587fba582ca434924f813b6a276f7040b1cca0
jmcraftgamer <jmcraftgamerplayerr@gmail.com>
2026-09-21 20:12:46 -0300

    fix: remove Android native splash icon, splash 800ms

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(milliseconds: 1500), () {
+    Future.delayed(const Duration(milliseconds: 800), () {
       if (mounted) {
         Navigator.pushReplacement(
           context,
